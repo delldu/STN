63ea70d57bd142ca96e76d32a0395895ec1c45f0
Dell Du <18588220928@163.com>
2019-12-27 21:06:41 +0800

    Add test.sh

---
+++ test.sh
@@ -0,0 +1,4 @@
+python mnist_train.py --model unbounded_stn --angle 90 --grid_size 4
+python mnist_visualize.py --model unbounded_stn --angle 90 --grid_size 4
+python mnist_make_gif.py --model unbounded_stn --angle 90 --grid_size 4
+
